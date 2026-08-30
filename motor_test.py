f7ce85d021638544df135644b08e3429a9d81a3a
Brandon Shelton <brandon.michael.shelton@gmail.com>
2026-08-29 18:02:09 -0700

    updated motor_test

--- motor_test.py
+++ motor_test.py
@@ -1,8 +1,12 @@
+from gpiozero.pins.lgpio import LGPIOFactory
+from gpiozero import Device
+Device.pin_factory = LGPIOFactory(chip=15)
+
 from gpiozero import Motor
 from time import sleep
 
-left = Motor(forward=17, backward=27, pwm=True)
-right = Motor(forward=23, backward=24, pwm=True)
+left = Motor(forward=17, backward=27, enable=22, pwm=True)
+right = Motor(forward=23, backward=24, enable=25, pwm=True)
 
 print("Left forward")
 left.forward(0.6)
